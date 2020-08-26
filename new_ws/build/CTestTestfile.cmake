61602e5163d0398c09e2392c0ea27fa1ebe69ecd
Jackson C <jacksonjjcai168@gmail.com>
2020-08-26 03:36:29 -0700

    backup

---
+++ new_ws/build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/javad/new_ws/src
+# Build directory: /home/javad/new_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(ur5/ur_description)
+subdirs(ur5/ur_gazebo)
+subdirs(world_env)
---
+++ new_ws/build/ur5/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/javad/new_ws/src/ur5
+# Build directory: /home/javad/new_ws/build/ur5
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
